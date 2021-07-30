d896bf26fee4f37697c13a7a317c204d105ca26e
samyuktha <samyukthapasupuleti585@gmail.com>
2021-07-30 20:50:41 +0530

    class-object commit

---
+++ Assignments-2/src/com/training/class_object/Bank.java
@@ -0,0 +1,20 @@
+package com.training.class_object;
+public class Bank {
+
+	double balance;
+	
+	Bank(double balance) {
+		this.balance=balance;
+	}
+	
+	public void withdraw(double x) {
+		balance=balance-x;
+	}
+	public void deposit(double x) {
+		balance=balance+x;
+	}
+	
+	double getBalance() {
+		return balance;
+	}
+}
---
+++ Assignments-2/src/com/training/class_object/Sample.java
@@ -0,0 +1,10 @@
+package com.training.class_object;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("sample");
+
+	}
+
+}
